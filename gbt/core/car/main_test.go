d0a8a4311b003b54db3c17dc23d0e3982c6b1884
Jiri Tyr <jiri.tyr@gmail.com>
2017-10-28 03:43:30 +0100

    Adding tests for core

--- gbt/core/car/main_test.go
+++ gbt/core/car/main_test.go
@@ -9,6 +9,219 @@ func getTestPrefix(testN int) string {
     return fmt.Sprintf("Test [%d]: ", testN)
 }
 
+func TestSetParamStr(t *testing.T) {
+    tests := []struct {
+        name string
+        value string
+        expectedValue string
+    }{
+        { name: "test", value: "aaa", expectedValue: "aaa", },
+    }
+
+    for i, test := range tests {
+        testPrefix := getTestPrefix(i)
+        car := Car{}
+
+        car.SetParamStr(test.name, test.value)
+
+        if test.value != car.Params[test.name] {
+            t.Errorf("%sExpected %x, found %x.", testPrefix, test.expectedValue, car.Params[test.name])
+        }
+    }
+}
+
+func TestGetModel(t *testing.T) {
+    tests := []struct {
+        key string
+        model map[string]ModelElement
+        expectedOutput map[string]ModelElement
+    }{
+        {
+            key:            "root",
+            model:          map[string]ModelElement{ "root": { Bg: "255", Fg: "red", Fm: "bold", Text: "test", }, },
+            expectedOutput: map[string]ModelElement{ "root": { Bg: "255", Fg: "red", Fm: "bold", Text: "test", }, },
+        },
+    }
+
+    for i, test := range tests {
+        testPrefix := getTestPrefix(i)
+        car := Car{
+            Model: test.model,
+        }
+
+        output := car.GetModel()
+
+        if output[test.key] != test.expectedOutput[test.key] {
+            t.Errorf("%sExpected %x, found %x.", testPrefix, test.expectedOutput, output)
+        }
+    }
+}
+
+func TestGetDisplay(t *testing.T) {
+    tests := []struct {
+        display bool
+        expectedOutput bool
+    }{
+        { display: true,  expectedOutput: true,  },
+        { display: false, expectedOutput: false, },
+    }
+
+    for i, test := range tests {
+        testPrefix := getTestPrefix(i)
+        car := Car{
+            Display: test.display,
+        }
+
+        output := car.GetDisplay()
+
+        if output != test.expectedOutput {
+            t.Errorf("%sExpected %x, found %x.", testPrefix, test.expectedOutput, output)
+        }
+    }
+}
+
+func TestSep(t *testing.T) {
+    tests := []struct {
+        sep string
+        expectedOutput string
+    }{
+        { sep: "x", expectedOutput: "x", },
+    }
+
+    for i, test := range tests {
+        testPrefix := getTestPrefix(i)
+        car := Car{
+            Sep: test.sep,
+        }
+
+        output := car.GetSep()
+
+        if output != test.expectedOutput {
+            t.Errorf("%sExpected %x, found %x.", testPrefix, test.expectedOutput, output)
+        }
+    }
+}
+
+func TestGetWrap(t *testing.T) {
+    tests := []struct {
+        wrap bool
+        expectedOutput bool
+    }{
+        { wrap: true,  expectedOutput: true,  },
+        { wrap: false, expectedOutput: false, },
+    }
+
+    for i, test := range tests {
+        testPrefix := getTestPrefix(i)
+        car := Car{
+            Wrap: test.wrap,
+        }
+
+        output := car.GetWrap()
+
+        if output != test.expectedOutput {
+            t.Errorf("%sExpected %x, found %x.", testPrefix, test.expectedOutput, output)
+        }
+    }
+}
+
+func TestFormat(t *testing.T) {
+    tests := []struct {
+        model map[string]ModelElement
+        expectedOutput string
+        display bool
+        shell string
+    }{
+        {
+            model: map[string]ModelElement{
+                "root": { Bg: "222", Fg: "red", Fm: "bold",    Text: "test", },
+            },
+            // TODO: This doesn't look correctly.
+            expectedOutput: "%{\x1b[48;5;222m%}%{\x1b[38;5;1m%}%{\x1b[1m%}%{\x1b[21m%}test",
+            display: true,
+            shell: "zsh",
+        },
+        /* TODO: Test cascading
+        {
+            model: map[string]ModelElement{
+                "root": { Bg: "222", Fg: "red", Fm: "bold",    Text: " {{ User }} ", },
+                "User": { Bg: "222", Fg: "red", Fm: "default", Text: "text", },
+            },
+            expectedOutput: "%{\x1b[48;5;222m%}%{\x1b[38;5;1m%}%{\x1b[1m%}text%{\x1b[21m%}",
+            display: true,
+            shell: "zsh",
+        },
+        */
+        {
+            model: map[string]ModelElement{ "root": { Bg: "222", Fg: "red", Fm: "bold", Text: "text", }, },
+            expectedOutput: "",
+            display: false,
+            shell: "zsh",
+        },
+    }
+
+    for i, test := range tests {
+        testPrefix := getTestPrefix(i)
+        Shell = test.shell
+        car := Car{
+            Model: test.model,
+            Display: test.display,
+        }
+
+        output := car.Format()
+
+        if output != test.expectedOutput {
+            t.Errorf("%sExpected %x, found %x.", testPrefix, test.expectedOutput, output)
+        }
+    }
+}
+
+func TestDecorateElement(t *testing.T) {
+    tests := []struct {
+        element string
+        model map[string]ModelElement
+        expectedOutput string
+        display bool
+        shell string
+    }{
+        {
+            element: "root",
+            model: map[string]ModelElement{ "root": { Bg: "222", Fg: "red", Fm: "bold", Text: "test", }, },
+            expectedOutput: "%{\x1b[48;5;222m%}%{\x1b[38;5;1m%}%{\x1b[1m%}%{\x1b[21m%}",
+            display: true,
+            shell: "zsh",
+        },
+        {
+            element: "User",
+            model: map[string]ModelElement{ "User": { Bg: "222", Fg: "red", Fm: "bold", Text: "test", }, },
+            expectedOutput: "%{\x1b[48;5;222m%}%{\x1b[38;5;1m%}%{\x1b[1m%}test%{\x1b[21m%}",
+            display: true,
+            shell: "zsh",
+        },
+    }
+
+    for i, test := range tests {
+        testPrefix := getTestPrefix(i)
+        Shell = test.shell
+        car := Car{
+            Model: test.model,
+            Display: test.display,
+        }
+
+        output := car.DecorateElement(
+            test.element,
+            test.model[test.element].Bg,
+            test.model[test.element].Fg,
+            test.model[test.element].Fm,
+            test.model[test.element].Text,
+        )
+
+        if output != test.expectedOutput {
+            t.Errorf("%sExpected %x, found %x.", testPrefix, test.expectedOutput, output)
+        }
+    }
+}
+
 func TestGetColor(t *testing.T) {
     tests := []struct {
         name string
@@ -16,26 +229,42 @@ func TestGetColor(t *testing.T) {
         expectedOutput string
         shell string
     }{
-        { name: "red", isFg: false, expectedOutput: "%{\x1b[48;5;1m%}", shell: "zsh", },
-        { name: "red", isFg: false, expectedOutput: "\001\x1b[48;5;1m\002", shell: "bash", },
-        { name: "red", isFg: true, expectedOutput: "%{\x1b[38;5;1m%}", shell: "zsh", },
-        { name: "red", isFg: true, expectedOutput: "\001\x1b[38;5;1m\002", shell: "bash", },
-        { name: "222", isFg: false, expectedOutput: "%{\x1b[48;5;222m%}", shell: "zsh", },
-        { name: "222", isFg: false, expectedOutput: "\001\x1b[48;5;222m\002", shell: "bash", },
-        { name: "222", isFg: true, expectedOutput: "%{\x1b[38;5;222m%}", shell: "zsh", },
-        { name: "222", isFg: true, expectedOutput: "\001\x1b[38;5;222m\002", shell: "bash", },
-        { name: "0;0;255", isFg: false, expectedOutput: "%{\x1b[48;2;0;0;255m%}", shell: "zsh", },
-        { name: "0;0;255", isFg: false, expectedOutput: "\001\x1b[48;2;0;0;255m\002", shell: "bash", },
-        { name: "0;0;255", isFg: true, expectedOutput: "%{\x1b[38;2;0;0;255m%}", shell: "zsh", },
-        { name: "0;0;255", isFg: true, expectedOutput: "\001\x1b[38;2;0;0;255m\002", shell: "bash", },
-        { name: "default", isFg: false, expectedOutput: "%{\x1b[49m%}", shell: "zsh", },
-        { name: "default", isFg: false, expectedOutput: "\001\x1b[49m\002", shell: "bash", },
-        { name: "default", isFg: true, expectedOutput: "%{\x1b[39m%}", shell: "zsh", },
-        { name: "default", isFg: true, expectedOutput: "\001\x1b[39m\002", shell: "bash", },
-        { name: "_unknown", isFg: false, expectedOutput: "%{\x1b[49m%}", shell: "zsh", },
-        { name: "_unknown", isFg: false, expectedOutput: "\001\x1b[49m\002", shell: "bash", },
-        { name: "_unknown", isFg: true, expectedOutput: "%{\x1b[39m%}", shell: "zsh", },
-        { name: "_unknown", isFg: true, expectedOutput: "\001\x1b[39m\002", shell: "bash", },
+        { name: "red",      isFg: false, expectedOutput: "%{\x1b[48;5;1m%}",           shell: "zsh",   },
+        { name: "red",      isFg: false, expectedOutput: "\001\x1b[48;5;1m\002",       shell: "bash",  },
+        { name: "red",      isFg: false, expectedOutput: "\\[\\e[48;5;1m\\]",          shell: "_bash", },
+        { name: "red",      isFg: true,  expectedOutput: "%{\x1b[38;5;1m%}",           shell: "zsh",   },
+        { name: "red",      isFg: true,  expectedOutput: "\001\x1b[38;5;1m\002",       shell: "bash",  },
+        { name: "red",      isFg: true,  expectedOutput: "\\[\\e[38;5;1m\\]",          shell: "_bash", },
+        { name: "222",      isFg: false, expectedOutput: "%{\x1b[48;5;222m%}",         shell: "zsh",   },
+        { name: "222",      isFg: false, expectedOutput: "\001\x1b[48;5;222m\002",     shell: "bash",  },
+        { name: "222",      isFg: false, expectedOutput: "\\[\\e[48;5;222m\\]",        shell: "_bash", },
+        { name: "222",      isFg: true,  expectedOutput: "%{\x1b[38;5;222m%}",         shell: "zsh",   },
+        { name: "222",      isFg: true,  expectedOutput: "\001\x1b[38;5;222m\002",     shell: "bash",  },
+        { name: "222",      isFg: true,  expectedOutput: "\\[\\e[38;5;222m\\]",        shell: "_bash", },
+        { name: "0;0;255",  isFg: false, expectedOutput: "%{\x1b[48;2;0;0;255m%}",     shell: "zsh",   },
+        { name: "0;0;255",  isFg: false, expectedOutput: "\001\x1b[48;2;0;0;255m\002", shell: "bash",  },
+        { name: "0;0;255",  isFg: false, expectedOutput: "\\[\\e[48;2;0;0;255m\\]",    shell: "_bash", },
+        { name: "0;0;255",  isFg: true,  expectedOutput: "%{\x1b[38;2;0;0;255m%}",     shell: "zsh",   },
+        { name: "0;0;255",  isFg: true,  expectedOutput: "\001\x1b[38;2;0;0;255m\002", shell: "bash",  },
+        { name: "0;0;255",  isFg: true,  expectedOutput: "\\[\\e[38;2;0;0;255m\\]",    shell: "_bash", },
+        { name: "default",  isFg: false, expectedOutput: "%{\x1b[49m%}",               shell: "zsh",   },
+        { name: "default",  isFg: false, expectedOutput: "\001\x1b[49m\002",           shell: "bash",  },
+        { name: "default",  isFg: false, expectedOutput: "\\[\\e[49m\\]",              shell: "_bash", },
+        { name: "default",  isFg: true,  expectedOutput: "%{\x1b[39m%}",               shell: "zsh",   },
+        { name: "default",  isFg: true,  expectedOutput: "\001\x1b[39m\002",           shell: "bash",  },
+        { name: "default",  isFg: true,  expectedOutput: "\\[\\e[39m\\]",              shell: "_bash", },
+        { name: "RESETALL", isFg: false, expectedOutput: "%{\x1b[0m%}",                shell: "zsh",   },
+        { name: "RESETALL", isFg: false, expectedOutput: "\001\x1b[0m\002",            shell: "bash",  },
+        { name: "RESETALL", isFg: false, expectedOutput: "\\[\\e[0m\\]",               shell: "_bash", },
+        { name: "RESETALL", isFg: true,  expectedOutput: "%{\x1b[0m%}",                shell: "zsh",   },
+        { name: "RESETALL", isFg: true,  expectedOutput: "\001\x1b[0m\002",            shell: "bash",  },
+        { name: "RESETALL", isFg: true,  expectedOutput: "\\[\\e[0m\\]",               shell: "_bash", },
+        { name: "_unknown", isFg: false, expectedOutput: "%{\x1b[49m%}",               shell: "zsh",   },
+        { name: "_unknown", isFg: false, expectedOutput: "\001\x1b[49m\002",           shell: "bash",  },
+        { name: "_unknown", isFg: false, expectedOutput: "\\[\\e[49m\\]",              shell: "_bash", },
+        { name: "_unknown", isFg: true,  expectedOutput: "%{\x1b[39m%}",               shell: "zsh",   },
+        { name: "_unknown", isFg: true,  expectedOutput: "\001\x1b[39m\002",           shell: "bash",  },
+        { name: "_unknown", isFg: true,  expectedOutput: "\\[\\e[39m\\]",              shell: "_bash", },
     }
 
     car := Car{
@@ -61,22 +290,30 @@ func TestGetFormat(t *testing.T) {
         expectedOutput string
         shell string
     }{
-        { name: "bold", isEnd: false, expectedOutput: "%{\x1b[01m%}", shell: "zsh", },
-        { name: "bold", isEnd: false, expectedOutput: "\001\x1b[01m\002", shell: "bash", },
-        { name: "bold", isEnd: true, expectedOutput: "%{\x1b[21m%}", shell: "zsh", },
-        { name: "bold", isEnd: true, expectedOutput: "\001\x1b[21m\002", shell: "bash", },
-        { name: "underline", isEnd: false, expectedOutput: "%{\x1b[04m%}", shell: "zsh", },
-        { name: "underline", isEnd: false, expectedOutput: "\001\x1b[04m\002", shell: "bash", },
-        { name: "underline", isEnd: true, expectedOutput: "%{\x1b[24m%}", shell: "zsh", },
-        { name: "underline", isEnd: true, expectedOutput: "\001\x1b[24m\002", shell: "bash", },
-        { name: "blink", isEnd: false, expectedOutput: "%{\x1b[05m%}", shell: "zsh", },
-        { name: "blink", isEnd: false, expectedOutput: "\001\x1b[05m\002", shell: "bash", },
-        { name: "blink", isEnd: true, expectedOutput: "%{\x1b[25m%}", shell: "zsh", },
-        { name: "blink", isEnd: true, expectedOutput: "\001\x1b[25m\002", shell: "bash", },
-        { name: "none", isEnd: false, expectedOutput: "%{%}", shell: "zsh", },
-        { name: "none", isEnd: false, expectedOutput: "\001\002", shell: "bash", },
-        { name: "none", isEnd: true, expectedOutput: "%{%}", shell: "zsh", },
-        { name: "none", isEnd: true, expectedOutput: "\001\002", shell: "bash", },
+        { name: "bold",      isEnd: false, expectedOutput: "%{\x1b[1m%}",      shell: "zsh",   },
+        { name: "bold",      isEnd: false, expectedOutput: "\001\x1b[1m\002",  shell: "bash",  },
+        { name: "bold",      isEnd: false, expectedOutput: "\\[\\e[1m\\]",     shell: "_bash", },
+        { name: "bold",      isEnd: true,  expectedOutput: "%{\x1b[21m%}",     shell: "zsh",   },
+        { name: "bold",      isEnd: true,  expectedOutput: "\001\x1b[21m\002", shell: "bash",  },
+        { name: "bold",      isEnd: true,  expectedOutput: "\\[\\e[21m\\]",    shell: "_bash", },
+        { name: "underline", isEnd: false, expectedOutput: "%{\x1b[4m%}",      shell: "zsh",   },
+        { name: "underline", isEnd: false, expectedOutput: "\001\x1b[4m\002",  shell: "bash",  },
+        { name: "underline", isEnd: false, expectedOutput: "\\[\\e[4m\\]",     shell: "_bash", },
+        { name: "underline", isEnd: true,  expectedOutput: "%{\x1b[24m%}",     shell: "zsh",   },
+        { name: "underline", isEnd: true,  expectedOutput: "\001\x1b[24m\002", shell: "bash",  },
+        { name: "underline", isEnd: true,  expectedOutput: "\\[\\e[24m\\]",    shell: "_bash", },
+        { name: "blink",     isEnd: false, expectedOutput: "%{\x1b[5m%}",      shell: "zsh",   },
+        { name: "blink",     isEnd: false, expectedOutput: "\001\x1b[5m\002",  shell: "bash",  },
+        { name: "blink",     isEnd: false, expectedOutput: "\\[\\e[5m\\]",     shell: "_bash", },
+        { name: "blink",     isEnd: true,  expectedOutput: "%{\x1b[25m%}",     shell: "zsh",   },
+        { name: "blink",     isEnd: true,  expectedOutput: "\001\x1b[25m\002", shell: "bash",  },
+        { name: "blink",     isEnd: true,  expectedOutput: "\\[\\e[25m\\]",    shell: "_bash", },
+        { name: "none",      isEnd: false, expectedOutput: "%{%}",             shell: "zsh",   },
+        { name: "none",      isEnd: false, expectedOutput: "\001\002",         shell: "bash",  },
+        { name: "none",      isEnd: false, expectedOutput: "\\[\\]",           shell: "_bash", },
+        { name: "none",      isEnd: true,  expectedOutput: "%{%}",             shell: "zsh",   },
+        { name: "none",      isEnd: true,  expectedOutput: "\001\002",         shell: "bash",  },
+        { name: "none",      isEnd: true,  expectedOutput: "\\[\\]",           shell: "_bash", },
     }
 
     car := Car{
--- gbt/core/utils/main_test.go
+++ gbt/core/utils/main_test.go
@@ -10,6 +10,33 @@ func getTestPrefix(testN int) string {
     return fmt.Sprintf("Test [%d]: ", testN)
 }
 
+func TestIsTrue(t *testing.T) {
+    tests := []struct {
+        input string
+        expectedOutput bool
+    }{
+        { input: "true", expectedOutput: true  },
+        { input: "True", expectedOutput: true  },
+        { input: "TRUE", expectedOutput: true  },
+        { input: "yes",  expectedOutput: true  },
+        { input: "Yes",  expectedOutput: true  },
+        { input: "YES",  expectedOutput: true  },
+        { input: "1",    expectedOutput: true  },
+        { input: "No",   expectedOutput: false },
+        { input: "0",    expectedOutput: false },
+    }
+
+    for i, test := range tests {
+        testPrefix := getTestPrefix(i)
+
+        output := IsTrue(test.input)
+
+        if output != test.expectedOutput {
+            t.Errorf("%sExpected '%s', found '%s'.", testPrefix, test.expectedOutput, output)
+        }
+    }
+}
+
 func TestGetEnv(t *testing.T) {
     tests := []struct {
         name string
@@ -17,8 +44,8 @@ func TestGetEnv(t *testing.T) {
         expectedVal string
         set bool
     }{
-        { name: "XXX", fallback: "", expectedVal: "", set: false, },
-        { name: "YYY", fallback: "", expectedVal: "test", set: true, },
+        { name: "XXX", fallback: "", expectedVal: "",     set: false, },
+        { name: "YYY", fallback: "", expectedVal: "test", set: true,  },
     }
 
     for i, test := range tests {
@@ -48,22 +75,22 @@ func TestGetEnvBool(t *testing.T) {
         set bool
         setVal string
     }{
-        { name: "XXX", fallback: false, expectedVal: false, set: false, setVal: "" },
-        { name: "YYY", fallback: true, expectedVal: true, set: true, setVal: "1" },
-        { name: "YYY", fallback: true, expectedVal: true, set: true, setVal: "true" },
-        { name: "YYY", fallback: true, expectedVal: true, set: true, setVal: "True" },
-        { name: "YYY", fallback: true, expectedVal: true, set: true, setVal: "TRUE" },
-        { name: "YYY", fallback: true, expectedVal: true, set: true, setVal: "yes" },
-        { name: "YYY", fallback: true, expectedVal: true, set: true, setVal: "Yes" },
-        { name: "YYY", fallback: true, expectedVal: true, set: true, setVal: "YES" },
-        { name: "YYY", fallback: true, expectedVal: false, set: true, setVal: "0" },
-        { name: "YYY", fallback: true, expectedVal: false, set: true, setVal: "false" },
-        { name: "YYY", fallback: true, expectedVal: false, set: true, setVal: "False" },
-        { name: "YYY", fallback: true, expectedVal: false, set: true, setVal: "FALSE" },
-        { name: "YYY", fallback: true, expectedVal: false, set: true, setVal: "no" },
-        { name: "YYY", fallback: true, expectedVal: false, set: true, setVal: "No" },
-        { name: "YYY", fallback: true, expectedVal: false, set: true, setVal: "NO" },
-        { name: "YYY", fallback: true, expectedVal: false, set: true, setVal: "UNKNOWN" },
+        { name: "XXX", fallback: false, expectedVal: false, set: false, setVal: ""       },
+        { name: "YYY", fallback: true,  expectedVal: true,  set: true, setVal: "1"       },
+        { name: "YYY", fallback: true,  expectedVal: true,  set: true, setVal: "true"    },
+        { name: "YYY", fallback: true,  expectedVal: true,  set: true, setVal: "True"    },
+        { name: "YYY", fallback: true,  expectedVal: true,  set: true, setVal: "TRUE"    },
+        { name: "YYY", fallback: true,  expectedVal: true,  set: true, setVal: "yes"     },
+        { name: "YYY", fallback: true,  expectedVal: true,  set: true, setVal: "Yes"     },
+        { name: "YYY", fallback: true,  expectedVal: true,  set: true, setVal: "YES"     },
+        { name: "YYY", fallback: true,  expectedVal: false, set: true, setVal: "0"       },
+        { name: "YYY", fallback: true,  expectedVal: false, set: true, setVal: "false"   },
+        { name: "YYY", fallback: true,  expectedVal: false, set: true, setVal: "False"   },
+        { name: "YYY", fallback: true,  expectedVal: false, set: true, setVal: "FALSE"   },
+        { name: "YYY", fallback: true,  expectedVal: false, set: true, setVal: "no"      },
+        { name: "YYY", fallback: true,  expectedVal: false, set: true, setVal: "No"      },
+        { name: "YYY", fallback: true,  expectedVal: false, set: true, setVal: "NO"      },
+        { name: "YYY", fallback: true,  expectedVal: false, set: true, setVal: "UNKNOWN" },
     }
 
     for i, test := range tests {
@@ -74,6 +101,70 @@ func TestGetEnvBool(t *testing.T) {
         }
 
         val := GetEnvBool(test.name, test.fallback)
+
+        if val != test.expectedVal {
+            t.Errorf("%sExpected '%s', found '%s'.", testPrefix, test.expectedVal, val)
+        }
+
+        if test.set {
+            os.Unsetenv(test.name)
+        }
+    }
+}
+
+func TestGetEnvInt(t *testing.T) {
+    tests := []struct {
+        name string
+        fallback int
+        expectedVal int
+        set bool
+        setVal string
+    }{
+        { name: "XXX", fallback: 1, expectedVal: 1, set: false, setVal: ""  },
+        { name: "YYY", fallback: 1, expectedVal: 2, set: true,  setVal: "2" },
+        { name: "YYY", fallback: 1, expectedVal: 1, set: true,  setVal: "t" },
+    }
+
+    for i, test := range tests {
+        testPrefix := getTestPrefix(i)
+
+        if test.set {
+            os.Setenv(test.name, test.setVal)
+        }
+
+        val := GetEnvInt(test.name, test.fallback)
+
+        if val != test.expectedVal {
+            t.Errorf("%sExpected '%s', found '%s'.", testPrefix, test.expectedVal, val)
+        }
+
+        if test.set {
+            os.Unsetenv(test.name)
+        }
+    }
+}
+
+func TestGetEnvFloat(t *testing.T) {
+    tests := []struct {
+        name string
+        fallback float64
+        expectedVal float64
+        set bool
+        setVal string
+    }{
+        { name: "XXX", fallback: 1.0, expectedVal: 1.0, set: false, setVal: ""    },
+        { name: "YYY", fallback: 1.0, expectedVal: 2.0, set: true,  setVal: "2.0" },
+        { name: "YYY", fallback: 1.0, expectedVal: 1.0, set: true,  setVal: "x.y" },
+    }
+
+    for i, test := range tests {
+        testPrefix := getTestPrefix(i)
+
+        if test.set {
+            os.Setenv(test.name, test.setVal)
+        }
+
+        val := GetEnvFloat(test.name, test.fallback)
 
         if val != test.expectedVal {
             t.Errorf("%sExpected '%s', found '%s'.", testPrefix, test.expectedVal, val)
