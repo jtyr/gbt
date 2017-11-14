bb381b672f7e8c80c2ba235d761eddd85e5f898f
Jiri Tyr <jiri.tyr@gmail.com>
2017-11-14 20:19:51 +0000

    Put package files into a versioned directory

--- dist/build.sh
+++ dist/build.sh
@@ -5,23 +5,26 @@ declare -a PLATFORMS=(
     'linux/amd64'
 )
 
-TMP='/tmp/gbt'
+NAME='gbt'
+VER="${TRAVIS_TAG:1}"
+TMP="/tmp/$NAME"
 rm -fr "$TMP"
 
 for P in "${PLATFORMS[@]}"; do
     echo "Building $P"
 
-    PTMP="$TMP/$P"
+    PTMP="$TMP/$P/$NAME-$VER"
     OS="${P%%/*}"
     ARCH="${P#*/}"
+    PKG="$NAME-$VER-$OS-$ARCH.tar.gz"
 
     mkdir -p "$PTMP"
-    GOOS="$OS" GOARCH="$ARCH" CGO_ENABLED=0 go build -ldflags='-s -w' -o "$PTMP/gbt"
+    GOOS="$OS" GOARCH="$ARCH" CGO_ENABLED=0 go build -ldflags='-s -w' -o "$PTMP/$NAME"
 
     (
         cp -r "$TRAVIS_BUILD_DIR"/{README.md,LICENSE,themes,sources} "$PTMP"
-        tar -C "$PTMP" -czf "$TMP/gbt-$TRAVIS_TAG-$OS-$ARCH.tar.gz" ./
+        tar -C "$PTMP/.." -czf "$TMP/$PKG" ./
         cd "$TMP"
-        sha256sum "gbt-$TRAVIS_TAG-$OS-$ARCH.tar.gz" >> "$TMP/gbt-$TRAVIS_TAG-checksums.txt"
+        sha256sum "$PKG" >> "$TMP/$NAME-$VER-checksums.txt"
     )
 done
