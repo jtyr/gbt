3cc9cd9399774c485832aefe9ecbab4ce5d606b9
Jiri Tyr <jiri.tyr@gmail.com>
2018-09-07 11:28:14 +0100

    Fixing variable reference

--- sources/gbts/cmd/remote/ssh.sh
+++ sources/gbts/cmd/remote/ssh.sh
@@ -7,7 +7,7 @@ function gbt_ssh() {
     [ -z $SSH_BIN ] && return 1
 
     $SSH_BIN -t "$@" "cat /etc/motd 2>/dev/null;
-echo \"$(cat $GBT__CONF | eval "$GBT__SOURCE_COMPRESS" | GBT__SOURCE_BASE64 | tr -d '\r\n')\" | $GBT__SOURCE_BASE64 $GBT__SOURCE_BASE64_DEC | $GBT__SOURCE_DECOMPRESS > $GBT__CONF &&
+echo \"$(cat $GBT__CONF | eval "$GBT__SOURCE_COMPRESS" | $GBT__SOURCE_BASE64 | tr -d '\r\n')\" | $GBT__SOURCE_BASE64 $GBT__SOURCE_BASE64_DEC | $GBT__SOURCE_DECOMPRESS > $GBT__CONF &&
 exec -a gbt.bash bash --rcfile $GBT__CONF;
 rm -f $GBT__CONF $GBT__CONF.bash"
 }
