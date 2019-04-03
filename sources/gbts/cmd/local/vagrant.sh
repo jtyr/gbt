abf0949a5e4483623b545288bfed22dc4d6238a6
Jiri Tyr <jiri.tyr@gmail.com>
2019-04-04 00:17:27 +0100

    Fixing variable name

--- sources/gbts/cmd/local/vagrant.sh
+++ sources/gbts/cmd/local/vagrant.sh
@@ -7,18 +7,18 @@ function gbt_vagrant() {
     else
         shift
 
-        local RDN=$RANDOM
-        local GBT__CONF="/tmp/.gbt.$RDN"
+        local RND=$RANDOM
+        local GBT__CONF="/tmp/.gbt.$RND"
 
         $VAGRANT_BIN ssh --command "cat /etc/motd 2>/dev/null;
 export GBT__CONF='$GBT__CONF' &&
 export GBT__CONF_BASH_MODE='$GBT__CONF_BASH_MODE';
-function gbt__$RDN() { echo '$((gbt__get_sources; echo 'gbt__ssh_theme') | eval "$GBT__SOURCE_COMPRESS" | $GBT__SOURCE_BASE64_LOCAL | tr -d '\r\n')' | $GBT__SOURCE_BASE64 $GBT__SOURCE_BASE64_DEC | $GBT__SOURCE_DECOMPRESS; };
+function gbt__$RND() { echo '$((gbt__get_sources; echo 'gbt__ssh_theme') | eval "$GBT__SOURCE_COMPRESS" | $GBT__SOURCE_BASE64_LOCAL | tr -d '\r\n')' | $GBT__SOURCE_BASE64 $GBT__SOURCE_BASE64_DEC | $GBT__SOURCE_DECOMPRESS; };
 if [ -z "$GBT__SOURCE_SEC_DISABLE" ]; then
-  export GBT__CONF_MD5=\$(gbt__$RDN | tee $GBT__CONF | $GBT__SOURCE_MD5_REMOTE 2>/dev/null | cut -d' ' -f$GBT__SOURCE_MD5_CUT_REMOTE 2>/dev/null);
+  export GBT__CONF_MD5=\$(gbt__$RND | tee $GBT__CONF | $GBT__SOURCE_MD5_REMOTE 2>/dev/null | cut -d' ' -f$GBT__SOURCE_MD5_CUT_REMOTE 2>/dev/null);
   echo '[ -z \"\$GBT__CONF_MD5\" ] && export GBT__CONF_MD5='\$GBT__CONF_MD5' || true' >> $GBT__CONF;
 else
-  gbt__$RDN > $GBT__CONF;
+  gbt__$RND > $GBT__CONF;
 fi;
 exec -a gbt.bash bash --rcfile \$GBT__CONF" "$@"
     fi
