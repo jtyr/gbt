1ff3099678969f8ea2ada0d51298c105367493b2
Jonas Forsberg <jonas.forsberg@suse.com>
2019-04-11 07:50:10 +0200

    added gbt__is_ssh_command check

--- sources/gbts/cmd/remote/ssh.sh
+++ sources/gbts/cmd/remote/ssh.sh
@@ -2,10 +2,13 @@ function gbt_ssh() {
     local SSH_BIN=$(gbt__which ssh)
     [ -z "$SSH_BIN" ] && return 1
 
-    gbt__check_md5
-
-    $SSH_BIN -t "$@" "cat /etc/motd 2>/dev/null;
+    if gbt__is_ssh_command "$@"; then
+        $SSH_BIN "$@"
+    else
+        gbt__check_md5
+        $SSH_BIN -t "$@" "cat /etc/motd 2>/dev/null;
 echo \"$(cat $GBT__CONF | eval "$GBT__SOURCE_COMPRESS" | $GBT__SOURCE_BASE64 | tr -d '\r\n')\" | $GBT__SOURCE_BASE64 $GBT__SOURCE_BASE64_DEC | $GBT__SOURCE_DECOMPRESS > $GBT__CONF &&
 chmod $GBT__CONF_MODE $GBT__CONF &&
 exec -a gbt.bash bash --rcfile $GBT__CONF"
+    fi
 }
