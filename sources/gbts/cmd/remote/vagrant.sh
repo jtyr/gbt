ec790dc29453161f49d6efdfd7a2f1e449caef6c
Jiri Tyr <jiri.tyr@gmail.com>
2019-04-13 21:22:14 +0100

    Fixing vagrant condition

--- sources/gbts/cmd/remote/vagrant.sh
+++ sources/gbts/cmd/remote/vagrant.sh
@@ -4,14 +4,14 @@ function gbt_vagrant() {
 
     gbt__check_md5
 
-    if [ "$1" != 'ssh' ] || ( gbt__is_vagrant_ssh_command "$@" ); then
-        $VAGRANT_BIN "$@"
-    else
+    if [[ $1 == 'ssh' ]] && ( ! gbt__is_vagrant_ssh_command "$@" ); then
         shift
 
         $VAGRANT_BIN ssh --command "cat /etc/motd 2>/dev/null;
 echo \"$(cat $GBT__CONF | eval "$GBT__SOURCE_COMPRESS" | $GBT__SOURCE_BASE64 | tr -d '\r\n')\" | $GBT__SOURCE_BASE64 $GBT__SOURCE_BASE64_DEC | $GBT__SOURCE_DECOMPRESS > $GBT__CONF &&
 chmod $GBT__CONF_MODE $GBT__CONF &&
 exec -a gbt.bash bash --rcfile $GBT__CONF" "$@"
+    else
+        $VAGRANT_BIN "$@"
     fi
 }
