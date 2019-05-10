7377935d791a71217f1b8c8566fef86d66c2b5c4
Rui Ventura <rventura.pt@outlook.com>
2019-05-10 23:03:07 +0100

    Remove GBTS scripts only if prompt forwarding was used with sudo

--- sources/gbts/cmd/local/sudo.sh
+++ sources/gbts/cmd/local/sudo.sh
@@ -4,6 +4,8 @@ function gbt_sudo() {
     local SUDO_BIN=$(gbt__which sudo)
     [ -z "$SUDO_BIN" ] && return 1
 
+    local rv
+
     if [ "$1" != 'su' ] && [[ " $@ " != *" -i "* ]]; then
         $SUDO_BIN "$@"
     else
@@ -12,11 +14,11 @@ function gbt_sudo() {
         local GBT__CONF=$(gbt__local_rcfile)
 
         $SUDO_BIN $SU_BIN -s "$GBT__CONF.bash" "$@"
+
+        rv=$?
+
+        rm -f $GBT__CONF $GBT__CONF.bash
     fi
 
-    local rv=$?
-
-    rm -f $GBT__CONF $GBT__CONF.bash
-
-    return $rv
+    return ${rv:-$?}
 }
