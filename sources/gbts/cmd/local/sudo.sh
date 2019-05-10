9d70dcbbe37b2bb50e286d6aea00318263ee58de
Rui Ventura <rventura.pt@outlook.com>
2019-05-10 01:38:22 +0100

    Remove GBT__LOCAL only when su is used

--- sources/gbts/cmd/local/sudo.sh
+++ sources/gbts/cmd/local/sudo.sh
@@ -4,19 +4,22 @@ function gbt_sudo() {
     local SUDO_BIN=$(gbt__which sudo)
     [ -z "$SUDO_BIN" ] && return 1
 
+    local rv
     if [ "$1" != 'su' ] && [[ " $@ " != *" -i "* ]]; then
         $SUDO_BIN "$@"
+
+        rv=$?
     else
         shift
 
         local GBT__CONF=$(gbt__local_rcfile)
 
         $SUDO_BIN $SU_BIN -s "$GBT__CONF.bash" "$@"
+
+        rv=$?
+        
+        rm -f $GBT__CONF $GBT__CONF.bash
     fi
-
-    local rv=$?
-
-    rm -f $GBT__CONF $GBT__CONF.bash
 
     return $rv
 }
