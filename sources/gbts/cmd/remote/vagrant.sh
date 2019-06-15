989319d7db50deaa8ef9b88fe0bf884581254c50
Jiri Tyr <jiri.tyr@gmail.com>
2019-06-15 23:35:11 +0100

    Allow to obtain shell even if /tmp is read only

--- sources/gbts/cmd/remote/vagrant.sh
+++ sources/gbts/cmd/remote/vagrant.sh
@@ -9,7 +9,7 @@ function gbt_vagrant() {
 
         $VAGRANT_BIN ssh --command "cat /etc/motd 2>/dev/null;
 echo \"$(cat $GBT__CONF | eval "$GBT__SOURCE_COMPRESS" | $GBT__SOURCE_BASE64 | tr -d '\r\n')\" | $GBT__SOURCE_BASE64 $GBT__SOURCE_BASE64_DEC | $GBT__SOURCE_DECOMPRESS > $GBT__CONF &&
-chmod $GBT__CONF_MODE $GBT__CONF &&
+chmod $GBT__CONF_MODE $GBT__CONF;
 exec -a gbt.bash bash --rcfile $GBT__CONF" "$@"
     else
         $VAGRANT_BIN "$@"
