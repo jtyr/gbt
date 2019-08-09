d44b5765d5201b7fec4f2b65d879ab1501b27c2c
Vladimir <vladimir.d.babichev@gmail.com>
2019-08-09 22:15:52 +0100

    Added mrdrup theme for local and remote prompts

---
+++ themes/mrdrup.sh
@@ -0,0 +1,33 @@
+export GBT_CARS='Time, Status, Hostname, Dir, Git, Sign'
+
+export GBT_CAR_BG='default'
+
+export GBT_CAR_DIR_FG='111'
+export GBT_CAR_DIR_FM='bold'
+export GBT_CAR_DIR_FORMAT='{{ Dir }}'
+
+export GBT_CAR_GIT_FG='220'
+export GBT_CAR_GIT_FM='bold'
+export GBT_CAR_GIT_FORMAT='{ {{ Icon }} {{ Head }}{{ Stash }}{{ StatusAdded }}{{ StatusDeleted }}{{ StatusModified }}{{ StatusUntracked }}{{ StatusStaged }}{{ Ahead }}{{ Behind }} }'
+
+export GBT_CAR_HOSTNAME_FG='204'
+export GBT_CAR_HOSTNAME_FORMAT='[{{ Host }}]'
+
+export GBT_CAR_SIGN_ADMIN_FG='light_red'
+export GBT_CAR_SIGN_ADMIN_TEXT='#'
+export GBT_CAR_SIGN_FORMAT='{{ Symbol }} '
+export GBT_CAR_SIGN_USER_FG='106'
+export GBT_CAR_SIGN_USER_FM='bold'
+export GBT_CAR_SIGN_USER_TEXT=$'\ue285'
+
+export GBT_CAR_STATUS_BG='default'
+export GBT_CAR_STATUS_DISPLAY='1'
+export GBT_CAR_STATUS_ERROR_FG='red'
+export GBT_CAR_STATUS_FORMAT='{{ Symbol }}'
+export GBT_CAR_STATUS_OK_FG='106'
+
+export GBT_CAR_TIME_FG='dark_gray'
+export GBT_CAR_TIME_FM='bold'
+export GBT_CAR_TIME_FORMAT='{{ Time }}'
+
+export GBT_SEPARATOR=' '
