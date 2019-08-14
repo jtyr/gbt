870d713de7f6251323d9ab97cfb5836c627482c4
mrdrup <mr.dr.up@gmail.com>
2019-08-14 23:26:00 +0100

    Added mrdrup theme for local and remote prompts (#39)

--- sources/gbts/theme/ssh/mrdrup.sh
+++ sources/gbts/theme/ssh/mrdrup.sh
@@ -1,17 +1,30 @@
-export GBT_CARS='Time, Os, Hostname, Dir, Sign'
-export GBT_CAR_BG='default'
-export GBT_SEPARATOR=''
+export GBT_CARS='Os, Time, Status, Hostname, Dir, Sign'
 
-export GBT_CAR_DIR_FG='light_blue'
+export GBT_CAR_BG='default'
+
+export GBT_CAR_DIR_FG='111'
 export GBT_CAR_DIR_FM='bold'
 export GBT_CAR_DIR_FORMAT='{{ Dir }}'
 export GBT_CAR_DIR_SEP=' '
 
-export GBT_CAR_HOSTNAME_FG='yellow'
+export GBT_CAR_HOSTNAME_ADMIN_FG='203'
+export GBT_CAR_HOSTNAME_ADMIN_FM='bold'
+export GBT_CAR_HOSTNAME_FG='204'
 export GBT_CAR_HOSTNAME_FORMAT='[{{ UserHost }}]'
-export GBT_CAR_HOSTNAME_USER_FG='green'
-export GBT_CAR_HOSTNAME_ADMIN_FG='red'
+export GBT_CAR_HOSTNAME_HOST_FG='221'
+export GBT_CAR_HOSTNAME_USER_FG='106'
+export GBT_CAR_HOSTNAME_USER_FM='bold'
+
+export GBT_CAR_SIGN_ADMIN_FG='203'
+export GBT_CAR_SIGN_USER_FG='106'
+
+export GBT_CAR_STATUS_BG='default'
+export GBT_CAR_STATUS_DISPLAY='1'
+export GBT_CAR_STATUS_ERROR_FG='red'
+export GBT_CAR_STATUS_OK_FG='106'
 
 export GBT_CAR_TIME_FG='dark_gray'
 export GBT_CAR_TIME_FM='bold'
 export GBT_CAR_TIME_FORMAT=' {{ Time }}'
+
+export GBT_SEPARATOR=''
